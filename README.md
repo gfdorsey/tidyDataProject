29355db71bf3b2f76f1b3985603a8354b6d9adb5
George F. Dorsey, Jr <gfdorsey@gmail.com>
2015-04-25 22:57:04 -0400

    first full commit

--- README.md
+++ README.md
@@ -1,2 +1,94 @@
 # tidyDataProject
 Coursera project for Getting and Cleaning Data course
+
+## Project Purpose
+Create a tidy set of summary data from various data files in the
+"Human Activity Recognition Using Smartphones Data Set" on the
+UC Irvine Machine Learning Repository.  
+
+The data are available from the link supplied on the course website: 
+[https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip]  
+
+
+## Requirements Specified in the Project Description
+
+1. Merges the training and the test sets to create one data set.
+2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+3. Uses descriptive activity names to name the activities in the data set
+4. Appropriately labels the data set with descriptive variable names. 
+5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+
+## Setup and Requirements
+
+The script requires the dplyr library, and requires that the data set referenced
+above is in a directory called dataset in the same directory as the script
+run_analysis.R.  
+
+After running the script on the original data, the resulting file mean.dat can
+be read back into R using:
+```{r eval = FALSE}
+mean.data <- read.table("mean.dat")
+```
+The mean.data data frame is preserved after running the run_analysis.R script,
+but all other temporary variables are removed from memory.
+
+
+## Script Description
+
+### Step 1: Read the data into temporary data frames
+The data from each file are loaded into a separate data frame.
+
+### Step 2: Combine test and training data
+The X, y, and subject data are combined pairwise, and the original data
+are removed from memory. This paritally completes requirment 1, but it is
+convenient to do more processing on the data frames _before_ the final column
+bind.
+
+### Step 3: Add column headers and make them human readable
+The features.txt file provides the names of the columns, which are omitted from
+the "raw" data sets (actually data originally processed by the contributor---see
+the Readme.txt file in the zip file referenced above for details).  After
+applying these names to the X data frame, a series of substitutions are done to
+make the names more meaninful.  This fulfills requirement 4 for the X data set.
+
+### Step 4: Select column headers
+Column headers are selected if they meet the specified criteria.
+The data we are interested in are only the mean() and std() summary data.  These
+were renamed to ...Mean... and ...StandardDeviation... in step 3.  Because the
+angle data we are not interested in also contains the string "Mean", selection
+is a 2 step filter using the grepl command---once to deselect the angle data and
+once to select all columns with either Mean or StandardDeviation in their names.
+The logical and of these vectors produces the desired column selection vector.
+This completes requirement 2.
+
+### Step 5: Tidy the 'y' data set
+To tidy the 'y' data set, the numeric factors need to be converted to
+descriptive names as specified by requirement 3.  These names are found in the
+activitiy_labels.txt file and are converted from VARIABLE_NAMES to readable
+text.
+
+### Step 6: Complete the merge
+Column names are added to the 'y' and 'subject' data frames, and these data
+frames are merged with the x data frame, completing requirements 1 and 4.  At
+This point, the first 4 requirements are complete, but multiple observations
+for a given pair of subject and activity need to be averaged for each of the
+other variables.
+
+### Step 7: Create the final tidy data set
+Various dplyr and plyr functions were tried but did give suitable results.  A
+rather brute force method was therefore used to complete the project by the
+deadline.  Feedback is welcome on other approaches that would be simpler.
+The data were split by subject and then each list item was further split by
+activity.  Use of the `r colMeans` function then provided a vector of the 66
+variables selected in step 4.  These vectors were then recombined into the final
+data set.
+
+### Step 8: Save the data
+The data were saved using the `r write.table` command so they could then be
+retrieved later.  The mean.data data frame is also retained in memory, but
+all other temporary variables are purged to save memory space.
+
+
+
+
---
+++ run_analysis.R
@@ -0,0 +1,138 @@
+## Coursera / Johns Hopkins
+## Data Science
+## 03 Getting and Cleaning Data
+##
+## George F. Dorsey, Jr.
+##
+## Project 1
+
+
+## 0: Packages Used ####################################################
+require(dplyr)
+
+## 1: Read Data ########################################################
+## The script assumes that the data from the UCI Machine Learning
+## library have been unzipped into a folder called dataset in the
+## same directory as the script.
+
+subject.test <- read.table("dataset/test/subject_test.txt")
+x.test <- read.table("dataset/test/X_test.txt")
+y.test <- read.table("dataset/test/y_test.txt")
+
+subject.train <- read.table("dataset/train/subject_train.txt")
+x.train <- read.table("dataset/train/X_train.txt")
+y.train <- read.table("dataset/train/y_train.txt")
+
+
+## 2: Merge Test and Train #############################################
+## Partially satisfies requirement 1.  The three sets will be
+## completely merged after requirements 2-4 are satisfied.
+subject <- rbind(subject.test,subject.train)
+x <- rbind(x.test, x.train)
+y <- rbind(y.test, y.train)
+rm(subject.test, subject.train, x.test, x.train, y.test, y.train)
+
+
+## 3: Name Columns for 'X' Set #########################################
+## Obtains the column names from the features.txt file and changes
+## them to meaningful values before setting the column names
+## of the data frame.
+## This satisfies requirment 4 for the 'X' set.
+
+features <- read.table("dataset/features.txt")
+x.colnames <- as.vector(sapply(features[2], function(x) {
+    # fix a small problem with some of the feature names
+    x <- sub("BodyBody", "Body", x)
+    
+    # convert to meaningful human readable names
+    x <- sub("tBodyAcc", "bodyAccelerometerSignal", x)
+    x <- sub("fBodyAcc", "bodyAccelerometerFFT", x)
+    x <- sub("tGravityAcc", "gravityAccelerometerSignal", x)
+    x <- sub("tBodyGyro", "bodyGyroscopeSignal", x)
+    x <- sub("fBodyGyro", "bodyGyroscopeFFT", x)
+    x <- sub("Mag", "Magnitude", x)
+    x <- sub("mean\\(\\)", "Mean", x)
+    x <- sub("std\\(\\)", "StandardDeviation", x)
+    # all other functions, and angle, will not be selected,
+    # so they are skipped
+    
+    # remove punctuation
+    x <- gsub("[^A-Za-z0-9]", "", x)
+    x
+}))
+
+colnames(x) <- x.colnames
+
+## 4: Select Relevant Columns ##########################################
+## Selects the relevant columns, namely any containing Mean
+## (formerly mean()) and StandardDeviation (formerly std()),
+## ignoring the "angle" variables which are not relevant for
+## this analysis.  Satisfies requirement 2
+
+selector1 = !grepl("angle", x.colnames)
+selector2 = grepl("Mean|StandardDeviation", x.colnames)
+x <- x[,selector1 & selector2]
+rm(features, x.colnames, selector1, selector2)
+
+## 5: Convert 'y' Set to Descriptive Names #############################
+## Satisfies requirment 3.
+## Uses activity labels file, after converting to camelCase and
+## making "LAYING" clearer about its purpose.
+
+activity.labels <- read.table("dataset/activity_labels.txt")
+y.values <- sapply(activity.labels, function(x) {
+    x <- tolower(x)
+    x <- sub("_", " ", x)
+    x <- sub("laying", "lying down", x)
+    x
+})
+y <- sapply(y, function(x) {
+    x <- y.values[x,2]
+})
+rm(activity.labels, y.values)
+
+
+## 6: Set Remaining Column Names and Complete Merge ####################
+## Completes requirments 1 and 4.
+
+colnames(y) <- "activity"
+colnames(subject) <- "subjectID"
+
+combined.data <- cbind(subject, y, x)
+combined.data <- arrange(combined.data, subjectID, activity)
+rm(subject, y, x)
+
+
+## 7: Calculate Column Means by Subject and Activity ###################
+## Implements step 5
+## It works.  Looking forward to finding a less brute force method, but
+## have to time to do so.
+
+tmp <- split(combined.data, combined.data$subjectID)
+tmp2 <- lapply(tmp, function(x) {
+    tmp.x <- split(x, x$activity)
+    list <- lapply(tmp.x, function(y) {
+        colMeans(y[,3:68])
+    })
+    list
+})
+
+names <- colnames(combined.data)
+mean.data <- data.frame()
+for (i in 1:30) {
+    tmp3 <- data.frame()
+    for (j in 1:6) {
+        tmp3 <- rbind(tmp3, tmp2[[i]][[j]])
+    }
+    tmp3 <- cbind(rep(i,6),attributes(tmp2[[1]])$names,tmp3)
+    colnames(tmp3) <- names
+    mean.data <- rbind(mean.data,tmp3)
+}
+rm(tmp, tmp2, tmp3, i, j, names, combined.data)
+
+## 8: Writes Tidy Data File ############################################
+## Finally, writes the data file to the working directory
+## as mean.dat.
+
+write.table(mean.data, "mean.dat")
+
